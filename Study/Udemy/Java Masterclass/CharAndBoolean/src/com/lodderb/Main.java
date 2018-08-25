a50688890cfe2942c340ef84c8c06922ab44c954
Buddy Lodder <b.lodder@bl-ict.nl>
2018-08-25 18:46:08 +0200

    - CharAndBoolean - Strings

---
+++ Study/Udemy/Java Masterclass/CharAndBoolean/src/com/lodderb/Main.java
@@ -0,0 +1,12 @@
+package com.lodderb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A9';
+        System.out.println("Unicode: " + myChar);
+        boolean myBoolean = true;
+        char anotherChar = '\u00AE';
+        System.out.println(anotherChar);
+    }
+}
---
+++ Study/Udemy/Java Masterclass/Strings/src/com/lodderb/Main.java
@@ -0,0 +1,31 @@
+package com.lodderb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2018";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.05";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+    }
+}
