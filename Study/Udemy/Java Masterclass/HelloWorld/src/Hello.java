4edf794e638f26c12a7ffbb5c43fc3d4f8554c91
Buddy Lodder <b.lodder@outlook.com>
2018-08-25 16:44:15 +0200

    First intro to operators and variables.

--- Study/Udemy/Java Masterclass/HelloWorld/src/Hello.java
+++ Study/Udemy/Java Masterclass/HelloWorld/src/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myNewTotal = 1000 - myTotal;
+
         System.out.println("Hello World!");
+        System.out.println(myTotal);
+        System.out.println(myNewTotal);
     }
 }
