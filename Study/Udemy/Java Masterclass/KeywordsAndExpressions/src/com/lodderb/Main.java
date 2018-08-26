29305c07e8f15b1189f518dd9e8f40bf8a01fd12
Buddy Lodder <b.lodder@bl-ict.nl>
2018-08-26 18:44:03 +0200

    KeywordsAndExpressions

---
+++ Study/Udemy/Java Masterclass/KeywordsAndExpressions/src/com/lodderb/Main.java
@@ -0,0 +1,34 @@
+package com.lodderb;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // a mile is equal to 1.609344 km
+        double kilometres = (100 * 1.609344);
+        // in this case "kilometres = (100 * 1.609344)" is the expression
+
+        int highScore = 50;
+        // in this case "highScore = 50" is the expression
+        if(highScore == 50)
+        // in this case "highScore == 50" is an expression
+        {
+            System.out.println("This is an expression");
+            // in this case " "This is an expression" " is an expression
+        }
+
+        // In the following code I will type below, write down what parts of the code
+        // Are expressions.
+        int score = 100;
+        if (score > 90) {
+            System.out.printf("You got the high score!");
+            score = 0;
+        }
+        // score = 100
+        // score > 90
+        // "You got the high score!"
+        // score = 0
+
+
+    }
+}
