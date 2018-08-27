fd2814d5e3f8dc3c996763ef7a53ccaf88226ae0
Buddy Lodder <b.lodder@outlook.com>
2018-08-27 13:43:29 +0200

    Added alternate solution to methods

--- Study/Udemy/Java Masterclass/Methods/src/com/company/Main.java
+++ Study/Udemy/Java Masterclass/Methods/src/com/company/Main.java
@@ -65,14 +65,24 @@ public class Main
 
     public static int calculatedHighScorePosition(int playerScore)
     {
-        if (playerScore >=1000)
-            return 1;
-        else if (playerScore >= 500 )
-            return 2;
-        else if (playerScore >= 100 )
-            return 3;
-        else
-            return 4;
+//        if (playerScore >=1000)
+//            return 1;
+//        else if (playerScore >= 500 )
+//            return 2;
+//        else if (playerScore >= 100 )
+//            return 3;
+//
+//            return 4;
+        int position = 4; // assuming position 4 will be returned
+
+        if (playerScore >= 1000)
+            position = 1;
+        else if (playerScore >= 500)
+            position = 2;
+        else if (playerScore >= 100)
+            position = 3;
+
+        return position;
     }
 
     public static int calculatedScore(boolean gameOver, int score, int levelCompleted, int bonus)
