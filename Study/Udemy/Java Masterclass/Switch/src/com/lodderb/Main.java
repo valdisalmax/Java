91d77c09e753bee3d20198cd5ef432a8f02bdbd6
Buddy Lodder <b.lodder@bl-ict.nl>
2018-09-28 13:05:25 +0200

    Switch and Else if

---
+++ Study/Udemy/Java Masterclass/Switch/src/com/lodderb/Main.java
@@ -0,0 +1,69 @@
+package com.lodderb;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 3;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+        }
+
+        char switchChar = 'D';
+
+        switch (switchChar) {
+            case 'A':
+                System.out.println("Char was A");
+                break;
+            case 'B':
+                System.out.println("Char was B");
+                break;
+            case 'C':
+            case 'D':
+            case 'E':
+                System.out.println("Char was " + switchChar);
+                break;
+
+            default:
+                System.out.println("Char A, B, C, D or E not found");
+                break;
+        }
+
+        String month = "June";
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+
+            default:
+                System.out.println("Not sure");
+        }
+
+    }
+}
